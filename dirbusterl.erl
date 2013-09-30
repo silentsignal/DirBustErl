ad8e9ef25c124260f98a922c2c0ec19c8a10ac37
András Veres-Szentkirályi <vsza@vsza.hu>
2013-09-30 16:57:43 +0200

    use one process per request

--- dirbusterl.erl
+++ dirbusterl.erl
@@ -2,18 +2,23 @@
 -compile([export_all]).
 
 -define(WORDLIST, "/home/dnet/_app/DirBuster-0.12/directory-list-2.3-small.txt").
--define(URL_PACKETS, 16).
 -define(WORKERS, 16).
 -define(TRIES, 4).
 
 bust(URL) ->
 	{ok, WordList} = file:open(?WORDLIST, [read, raw, read_ahead, binary]),
+	%%process_flag(trap_exit, true),
 	BaseURL = ensure_ends_with_slash(URL),
-	Workers = [spawn_link(dirbusterl, worker, [self(), BaseURL, get_packet(WordList)])
-			   || _ <- lists:seq(1, ?WORKERS)],
-	burst_wordlist(WordList),
-	[Pid ! quit || Pid <- Workers],
+	spawn_worker(BaseURL),
+	NProcs = burst_wordlist(BaseURL, WordList, 1),
+	io:format("~p\n", [NProcs]),
+	wait_for_procs(NProcs),
 	file:close(WordList).
+
+wait_for_procs(0) -> ok;
+wait_for_procs(NProcs) ->
+	receive {'DOWN', _, _, _, _} -> ok end,
+	wait_for_procs(NProcs - 1).
 
 ensure_ends_with_slash(Str) ->
 	case lists:reverse(Str) of
@@ -21,58 +26,47 @@ ensure_ends_with_slash(Str) ->
 		 WithoutSlash -> lists:reverse([$/ | WithoutSlash])
 	end.
 
-burst_wordlist(WordList) ->
-	receive
-		{ready, Pid} ->
-			case get_packet(WordList) of
-				<<>> -> ok;
-				Packet ->
-					Pid ! {packet, Packet},
-					burst_wordlist(WordList)
-			end;
-		Msg -> io:format("Unknown message in burst: ~p~n", [Msg])
-	end.
-
-get_packet(WordList) -> get_packet(WordList, ?URL_PACKETS, <<>>).
-get_packet(_, 0, Acc) -> Acc;
-get_packet(WordList, Count, Acc) ->
+burst_wordlist(BaseURL, WordList, N) ->
 	case file:read_line(WordList) of
+		{ok, <<$#, _/binary>>} -> burst_wordlist(BaseURL, WordList, N);
+		{ok, <<$\n>>} -> burst_wordlist(BaseURL, WordList, N);
 		{ok, Line} ->
-			NextAcc = case Line of
-						  <<$#, _/binary>> -> Acc; %% ignore comments
-						  _ -> <<Acc/binary, Line/binary>>
-					  end,
-			get_packet(WordList, Count - 1, NextAcc);
-		eof -> Acc
+			Postfix = binary_to_list(binary:replace(Line, <<$\n>>, <<>>)),
+			spawn_worker(BaseURL ++ ibrowse_lib:url_encode(Postfix)),
+			burst_wordlist(BaseURL, WordList, N + 1);
+		eof -> N
 	end.
 
-worker(Server, BaseURL, Packet) ->
-	try_urls(BaseURL, binary:split(Packet, <<$\n>>, [global, trim])),
-	Server ! {ready, self()},
-	receive
-		{packet, NewPacket} -> worker(Server, BaseURL, NewPacket);
-		quit -> ok;
-		Msg -> io:format("Unknown message in worker: ~p~n", [Msg])
-	end.
-
-try_urls(_, []) -> ok;
-try_urls(BaseURL, [Path | Rest]) ->
-	ReqURL = BaseURL ++ binary_to_list(Path),
-	try_url(ReqURL),
-	try_urls(BaseURL, Rest).
+spawn_worker(URL) ->
+	spawn_monitor(dirbusterl, try_url, [URL]).
 
 try_url(URL) -> try_url(URL, ?TRIES).
 try_url(URL, N) ->
 	case ibrowse:send_req(URL, [], get, [], [], infinity) of
 		{ok, "404", _, _} -> ok;
-		{ok, Code = [$3 | _], Headers, _} -> io:format("~s ~s -> ~s\n", [Code, URL, get_location(Headers)]);
+		{ok, Code = [$3 | _], Headers, _} ->
+			Location = get_location(Headers),
+			case get_location(Headers) of
+				no_location -> io:format("~s ~s\n", [Code, URL]);
+				Location ->
+					case is_dir_redir(URL, Location) of
+						true -> io:format("~s ~s [DIR]\n", [Code, URL]);
+						false -> io:format("~s ~s -> ~s\n", [Code, URL, get_location(Headers)])
+					end
+			end;
 		{ok, Code, _, _} -> io:format("~s ~s\n", [Code, URL]);
+		{error, retry_later} -> timer:sleep(100), try_url(URL, N);
 		{error, _} when N > 0 -> try_url(URL, N - 1)
 	end.
 
-get_location([]) -> "N/A";
+get_location([]) -> no_location;
 get_location([{Header, Value} | Rest]) ->
 	case string:to_lower(Header) of
 		"location" -> Value;
 		_ -> get_location(Rest)
 	end.
+
+is_dir_redir([C | URL], [C | Loc]) ->
+	is_dir_redir(URL, Loc);
+is_dir_redir("", "/") -> true;
+is_dir_redir(_, _) -> false.
