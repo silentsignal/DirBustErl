183de9e4f3cc24b656ecd3ae9697e6b9517d4b56
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-07 11:58:28 +0200

    added process limit and recursion

--- dirbusterl.erl
+++ dirbusterl.erl
@@ -2,26 +2,45 @@
 -compile([export_all]).
 
 -define(WORDLIST, "/home/dnet/_app/DirBuster-0.12/directory-list-2.3-small.txt").
--define(TRIES, 4).
+-define(TRIES, 8).
+-define(BACKOFF_LIMIT, 16384).
+-define(BACKOFF_INTERVAL, 64).
+-define(BACKOFF_MSEC, 3000).
 
 bust(URL) ->
-	{ok, WordList} = file:open(?WORDLIST, [read, raw, read_ahead, binary]),
-	%%process_flag(trap_exit, true),
-	BaseURL = ensure_ends_with_slash(URL),
 	Waiter = spawn_link(?MODULE, waiter, [self()]),
-	spawn_worker(BaseURL, Waiter),
-	burst_wordlist(BaseURL, WordList, Waiter),
-	file:close(WordList),
-	Waiter ! finished,
-	receive done -> done end.
+	{ok, WordList} = file:open(?WORDLIST, [read, raw, read_ahead, binary]),
+	bust(URL, Waiter, WordList),
+	file:close(WordList).
 
-waiter(Server) -> waiter(Server, 2).
+bust(URL, Waiter, WordList) ->
+	BaseURL = ensure_ends_with_slash(URL),
+	spawn_worker(BaseURL, Waiter),
+	file:position(WordList, bof),
+	burst_wordlist(BaseURL, WordList, Waiter),
+	Waiter ! finished,
+	receive
+		{bust, Target} -> bust(Target, Waiter, WordList);
+		done -> done
+	end.
+
+waiter(Server) -> waiter(Server, 1).
 waiter(Server, 0) -> Server ! done;
 waiter(Server, NProcs) ->
-	receive
-		started -> waiter(Server, NProcs + 1);
-		finished -> waiter(Server, NProcs - 1)
-	end.
+	NewProcs = receive
+		started -> NProcs + 1;
+		finished -> NProcs - 1;
+		{get, Pid} -> Pid ! {nprocs, NProcs}, NProcs;
+		{finished, URL, Code, Contents} ->
+			io:format("~s ~s\n", [Code, URL]),
+			case Contents of
+				dir -> Server ! {bust, URL};
+				{redir, Target} -> Server ! {bust, Target};
+				_ -> ok
+			end,
+			NProcs - 1
+	end,
+	waiter(Server, NewProcs).
 
 ensure_ends_with_slash(Str) ->
 	case lists:reverse(Str) of
@@ -30,38 +49,49 @@ ensure_ends_with_slash(Str) ->
 	end.
 
 burst_wordlist(BaseURL, WordList, Waiter) ->
+	burst_wordlist(BaseURL, WordList, Waiter, ?BACKOFF_INTERVAL).
+burst_wordlist(BaseURL, WordList, Waiter, 0) ->
+	Waiter ! {get, self()},
+	receive
+		{nprocs, N} when N > ?BACKOFF_LIMIT ->
+			timer:sleep(?BACKOFF_MSEC),
+			burst_wordlist(BaseURL, WordList, Waiter, 0);
+		{nprocs, _} -> burst_wordlist(BaseURL, WordList, Waiter)
+	end;
+burst_wordlist(BaseURL, WordList, Waiter, Check) ->
 	case file:read_line(WordList) of
-		{ok, <<$#, _/binary>>} -> burst_wordlist(BaseURL, WordList, Waiter);
-		{ok, <<$\n>>} -> burst_wordlist(BaseURL, WordList, Waiter);
+		{ok, <<$#, _/binary>>} -> burst_wordlist(BaseURL, WordList, Waiter, Check - 1);
+		{ok, <<$\n>>} -> burst_wordlist(BaseURL, WordList, Waiter, Check - 1);
 		{ok, Line} ->
 			Postfix = binary_to_list(binary:replace(Line, <<$\n>>, <<>>)),
-			Waiter ! started,
 			spawn_worker(BaseURL ++ ibrowse_lib:url_encode(Postfix), Waiter),
-			burst_wordlist(BaseURL, WordList, Waiter);
+			burst_wordlist(BaseURL, WordList, Waiter, Check - 1);
 		eof -> ok
 	end.
 
 spawn_worker(URL, Waiter) ->
+	Waiter ! started,
 	spawn_link(dirbusterl, try_url, [URL, Waiter]).
 
 try_url(URL, Waiter) -> try_url(URL, Waiter, ?TRIES).
 try_url(URL, Waiter, N) ->
 	case ibrowse:send_req(URL, [], get, [], [], infinity) of
 		{ok, "404", _, _} -> Waiter ! finished;
-		{ok, Code = [$3 | _], Headers, _} ->
+		{ok, Code = [$3 | _], Headers, Body} ->
 			Location = get_location(Headers),
-			case get_location(Headers) of
-				no_location -> io:format("~s ~s\n", [Code, URL]);
+			Payload = case get_location(Headers) of
+				no_location -> Body;
 				Location ->
 					case is_dir_redir(URL, Location) of
-						true -> io:format("~s ~s [DIR]\n", [Code, URL]);
-						false -> io:format("~s ~s -> ~s\n", [Code, URL, get_location(Headers)])
+						true -> dir;
+						false -> {redir, get_location(Headers)}
 					end
 			end,
-			Waiter ! finished;
-		{ok, Code, _, _} -> io:format("~s ~s\n", [Code, URL]), Waiter ! finished;
+			Waiter ! {finished, URL, Code, Payload};
+		{ok, Code, _, Body} -> Waiter ! {finished, URL, Code, Body};
 		{error, retry_later} -> timer:sleep(100), try_url(URL, Waiter, N);
-		{error, _} when N > 0 -> try_url(URL, Waiter, N - 1)
+		{error, _} when N > 0 -> try_url(URL, Waiter, N - 1);
+		{error, Reason} -> Waiter ! {finished, URL, error, Reason}
 	end.
 
 get_location([]) -> no_location;
