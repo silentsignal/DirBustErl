956d1ed6a55f3776425577f7fae771fe878e798f
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-14 17:18:52 +0200

    urljoin: more generalized absolute URL handler

--- url_tools.erl
+++ url_tools.erl
@@ -1,8 +1,8 @@
 -module(url_tools).
 -export([urljoin/2, subslashes/1, ensure_ends_with_slash/1]).
 
-urljoin(_, [$h, $t, $t, $p, $:, $/, $/ | _] = Path) -> Path;
-urljoin(_, [$h, $t, $t, $p, $s, $:, $/, $/ | _] = Path) -> Path;
+urljoin(Base, [$/, $/ | _] = Path) ->
+	re:replace(Base, "^([^:]+:)//.*$", "\\1" ++ Path, [{return, list}]);
 urljoin(Base, [$/ | _] = Path) ->
 	re:replace(Base, "^([^:]+://[^/]+)/.*$", "\\1" ++ Path, [{return, list}]);
 urljoin(Base, [$., $/ | Rest]) -> urljoin(Base, Rest);
