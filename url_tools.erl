11ebb06b4271dcbed4b0f61b394bd0a15b043644
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-14 19:38:35 +0200

    urljoin: handle abnormal cases

--- url_tools.erl
+++ url_tools.erl
@@ -7,8 +7,11 @@ urljoin(Base, [$/ | _] = Path) ->
 	re:replace(Base, "^([^:]+://[^/]+)/.*$", "\\1" ++ Path, [{return, list}]);
 urljoin(Base, [$., $/ | Rest]) ->
 	urljoin(lists:reverse(subslashes(lists:reverse(Base))), Rest);
-urljoin(Base, [$., $., $/ | Rest]) ->
-	urljoin(urljoin(Base, ".."), Rest);
+urljoin(Base, [$., $., $/ | Rest] = Path) ->
+	case has_at_least_three_slashes(Base) of
+		true -> urljoin(urljoin(Base, ".."), Rest);
+		false -> lists:reverse(subslashes(lists:reverse(Base)), Path)
+	end;
 urljoin(Base, [Sym | Rest]) when Sym =:= $#; Sym =:= $?; Sym =:= $; ->
 	case strip_symbol(lists:reverse(Base), Sym) of
 		not_found -> Base ++ Rest;
@@ -22,9 +25,35 @@ urljoin(Base, Path) ->
 	case lists:member($:, Path) of
 		true -> Path;
 		false ->
-			Parent = subslashes(lists:reverse(Base)),
-			lists:reverse(Parent, if Path =:= "." -> ""; true -> Path end)
+			{PathElem, Rest} = split_path(Path),
+			SubSlashed = subslashes(lists:reverse(Base)),
+			case is_special_path_elem(PathElem) of
+				true -> lists:reverse(SubSlashed, Path);
+				false -> urljoin(lists:reverse(SubSlashed, PathElem), Rest)
+			end
 	end.
+
+split_path(Path) ->
+	{Element, Rest} = split_path(Path, []),
+	{lists:reverse(Element), Rest}.
+split_path([] = L, Acc) -> {Acc, L};
+split_path([$/ | Path], Acc) -> {[$/ | Acc], Path};
+split_path([Char | Path], Acc) ->
+	split_path(Path, [Char | Acc]).
+
+is_special_path_elem([$? | _]) -> true;
+is_special_path_elem([$# | _]) -> true;
+is_special_path_elem([$; | _]) -> true;
+is_special_path_elem([_ | Rest]) -> is_special_path_elem(Rest);
+is_special_path_elem([]) -> false.
+
+has_at_least_three_slashes(URL) -> has_at_least_three_slashes(URL, 3).
+has_at_least_three_slashes("", _) -> false;
+has_at_least_three_slashes(_, 0) -> true;
+has_at_least_three_slashes([$/ | URL], N) ->
+	has_at_least_three_slashes(URL, N - 1);
+has_at_least_three_slashes([_ | URL], N) ->
+	has_at_least_three_slashes(URL, N).
 
 subslashes([$/ | _] = URL) -> URL;
 subslashes([_ | Rest]) -> subslashes(Rest).
