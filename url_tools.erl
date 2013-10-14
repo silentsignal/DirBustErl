0788c7f51fa70614964c1f78a075f5285a8285a9
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-14 17:21:15 +0200

    urljoin: improved handling of URLs having #$;. chars

--- url_tools.erl
+++ url_tools.erl
@@ -8,10 +8,27 @@ urljoin(Base, [$/ | _] = Path) ->
 urljoin(Base, [$., $/ | Rest]) -> urljoin(Base, Rest);
 urljoin(Base, [$., $., $/ | Rest]) ->
 	urljoin(lists:reverse(subslashes(tl(subslashes(lists:reverse(Base))))), Rest);
-urljoin(Base, Path) -> Base ++ Path.
+urljoin(Base, [Sym | Rest]) when Sym =:= $#; Sym =:= $?; Sym =:= $; ->
+	case strip_symbol(lists:reverse(Base), Sym) of
+		not_found -> Base ++ Rest;
+		Stripped -> lists:reverse(Stripped, Rest)
+	end;
+urljoin(Base, "..") ->
+	lists:reverse(subslashes(tl(subslashes(lists:reverse(Base)))));
+urljoin(Base, Path) ->
+	case lists:member($:, Path) of
+		true -> Path;
+		false ->
+			Parent = subslashes(lists:reverse(Base)),
+			lists:reverse(Parent, if Path =:= "." -> ""; true -> Path end)
+	end.
 
 subslashes([$/ | _] = URL) -> URL;
 subslashes([_ | Rest]) -> subslashes(Rest).
+
+strip_symbol([], _) -> not_found;
+strip_symbol([Symbol | _] = URL, Symbol) -> URL;
+strip_symbol([_ | Rest], Symbol) -> strip_symbol(Rest, Symbol).
 
 ensure_ends_with_slash(Str) ->
 	case lists:last(Str) of
