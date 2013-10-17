cbe7ec2b632420470b11a1d61f4d8ba39aab2ccb
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-17 11:53:14 +0200

    extracted record 'state'

--- src/dirbusterl.erl
+++ src/dirbusterl.erl
@@ -1,9 +1,12 @@
 -module(dirbusterl).
 -export([bust/2]).
+-record(state, {waiter, wordlist, config}).
 
 -define(BACKOFF_LIMIT, 16384).
 -define(BACKOFF_INTERVAL, 64).
 -define(BACKOFF_MSEC, 3000).
+
+-define(getStateConfigList(X), proplists:get_value(X, State#state.config, [])).
 
 bust(URL, UserConfig) ->
 	URLs = ets:new(dirbusterl_urls, [named_table]),
@@ -11,25 +14,26 @@ bust(URL, UserConfig) ->
 	Waiter = waiter:start_link(Config),
 	process_url_lists(Inputs, Waiter, filter_burst_config(Config)),
 	WordList = proplists:get_value(wordlist, Inputs),
-	bust(URL, dir, Waiter, WordList, Config),
+	bust(URL, dir, #state{waiter=Waiter, wordlist=WordList, config=Config}),
 	process_inputs_close(Inputs),
 	ets:delete(URLs).
 
-bust(URL, Mode, Waiter, WordList, Config) ->
-	spawn_worker(URL, Waiter, proplists:get_value(http_cfg, Config, [])),
+bust(URL, Mode, State) ->
+	spawn_worker(URL, State#state.waiter, ?getStateConfigList(http_cfg)),
 	case Mode of
 		dir ->
-			file:position(WordList, bof),
+			file:position(State#state.wordlist, bof),
 			BaseURL = url_tools:ensure_ends_with_slash(URL),
-			burst_wordlist(BaseURL, WordList, Waiter, filter_burst_config(Config));
+			burst_wordlist(BaseURL,
+				State#state{config=filter_burst_config(State#state.config)});
 		_ -> ok
 	end,
-	waiter:worker_finished(Waiter),
-	server_loop(Waiter, WordList, Config).
+	waiter:worker_finished(State#state.waiter),
+	server_loop(State).
 
 process_url_lists([], _, _) -> ok;
 process_url_lists([{url_list, URLList} | Inputs], Waiter, Config) ->
-	burst_wordlist(url_list, URLList, Waiter, Config),
+	burst_wordlist(url_list, #state{waiter=Waiter, wordlist=URLList, config=Config}),
 	process_url_lists(Inputs, Waiter, Config);
 process_url_lists([_ | Inputs], Waiter, Config) ->
 	process_url_lists(Inputs, Waiter, Config).
@@ -56,25 +60,25 @@ filter_burst_config([Item | Config], Acc)
 filter_burst_config([_ | Config], Acc) ->
 	filter_burst_config(Config, Acc).
 
-server_loop(Waiter, WordList, Config) ->
+server_loop(State) ->
 	receive
 		{bust_file, {Base, Path}} ->
 			BareBase = lists:reverse(url_tools:subslashes(lists:reverse(Base))),
-			try_bust(url_tools:urljoin(BareBase, Path), file, Waiter, WordList, Config);
+			try_bust(url_tools:urljoin(BareBase, Path), file, State);
 		{bust_file, Target} ->
-			try_bust(Target, file, Waiter, WordList, Config);
+			try_bust(Target, file, State);
 		{bust_dir, Target} ->
-			try_bust(Target, dir, Waiter, WordList, Config);
+			try_bust(Target, dir, State);
 		done -> done
 	end.
 
-try_bust(URL, Mode, Waiter, WordList, Config) ->
-	case url_allowed(URL, Config) of
+try_bust(URL, Mode, S) ->
+	case url_allowed(URL, S#state.config) of
 		true ->
-			waiter:worker_started(Waiter),
-			bust(URL, Mode, Waiter, WordList, Config);
+			waiter:worker_started(S#state.waiter),
+			bust(URL, Mode, S);
 		false ->
-			server_loop(Waiter, WordList, Config)
+			server_loop(S)
 	end.
 
 url_allowed(URL, Config) ->
@@ -93,36 +97,36 @@ process_url_restriction([{url_restriction, Restriction} | Config]) ->
 process_url_restriction([Item | Config]) -> [Item | process_url_restriction(Config)];
 process_url_restriction([]) -> [].
 
-burst_wordlist(BaseURL, WordList, Waiter, Config) ->
-	burst_wordlist(BaseURL, WordList, Waiter, Config, ?BACKOFF_INTERVAL).
-burst_wordlist(BaseURL, WordList, Waiter, Config, 0) ->
-	case waiter:get_nprocs(Waiter) of
+burst_wordlist(BaseURL, State) ->
+	burst_wordlist(BaseURL, State, ?BACKOFF_INTERVAL).
+burst_wordlist(BaseURL, State, 0) ->
+	case waiter:get_nprocs(State#state.waiter) of
 		N when N > ?BACKOFF_LIMIT ->
 			timer:sleep(?BACKOFF_MSEC),
-			burst_wordlist(BaseURL, WordList, Waiter, Config, 0);
-		_ -> burst_wordlist(BaseURL, WordList, Waiter, Config)
+			burst_wordlist(BaseURL, State, 0);
+		_ -> burst_wordlist(BaseURL, State)
 	end;
-burst_wordlist(BaseURL, WordList, Waiter, Config, Check) ->
-	NewCheck = case file:read_line(WordList) of
+burst_wordlist(BaseURL, State, Check) ->
+	NewCheck = case file:read_line(State#state.wordlist) of
 		{ok, <<$#, _/binary>>} -> Check;
 		{ok, <<$\n>>} -> Check;
 		{ok, Line} ->
 			Postfix = binary_to_list(Line, 1, byte_size(Line) - 1),
-			Params = proplists:get_value(http_cfg, Config, []),
+			Params = ?getStateConfigList(http_cfg),
 			case BaseURL of
-				url_list -> spawn_worker(Postfix, Waiter, Params);
+				url_list -> spawn_worker(Postfix, State#state.waiter, Params);
 				_ ->
-					UserPF = ["" | proplists:get_value(postfix, Config, [])],
+					UserPF = ["" | ?getStateConfigList(postfix)],
 					[spawn_worker(
 					   BaseURL ++ ibrowse_lib:url_encode(Postfix) ++ PF,
-					   Waiter, Params) || PF <- UserPF]
+					   State#state.waiter, Params) || PF <- UserPF]
 			end,
 			Check - 1;
 		eof -> ok
 	end,
 	case NewCheck of
 		ok -> ok;
-		_ -> burst_wordlist(BaseURL, WordList, Waiter, Config, NewCheck)
+		_ -> burst_wordlist(BaseURL, State, NewCheck)
 	end.
 
 spawn_worker(URL, Waiter, Params) ->
