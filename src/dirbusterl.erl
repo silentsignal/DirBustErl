0edee2d9b432a7655db026bac9ad169937881ed7
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-21 16:19:46 +0200

    merged process_{inputs_open,url_restriction}

--- src/dirbusterl.erl
+++ src/dirbusterl.erl
@@ -6,7 +6,7 @@
 
 bust(URL, UserConfig) ->
 	URLs = ets:new(dirbusterl_urls, []),
-	{Inputs, Config} = process_inputs_open(process_url_restriction(UserConfig)),
+	{Inputs, Config} = process_user_config(UserConfig),
 	Waiter = waiter:start_link(Config),
 	process_url_lists(Inputs, Waiter, filter_burst_config(Config)),
 	WordList = proplists:get_value(wordlist, Inputs),
@@ -26,15 +26,6 @@ process_url_lists([{url_list, URLList} | Inputs], Waiter, Config) ->
 process_url_lists([_ | Inputs], Waiter, Config) ->
 	process_url_lists(Inputs, Waiter, Config).
 
-process_inputs_open(Config) -> process_inputs_open(Config, [], []).
-process_inputs_open([], InputsAcc, ConfigAcc) -> {InputsAcc, ConfigAcc};
-process_inputs_open([{Type, FileName} | Config], InputsAcc, ConfigAcc)
-  when Type =:= wordlist; Type =:= url_list ->
-	{ok, FP} = file:open(FileName, [read, raw, read_ahead, binary]),
-	process_inputs_open(Config, [{Type, FP} | InputsAcc], ConfigAcc);
-process_inputs_open([Item | Config], InputsAcc, ConfigAcc) ->
-	process_inputs_open(Config, InputsAcc, [Item | ConfigAcc]).
-
 process_inputs_close([]) -> ok;
 process_inputs_close([{_, FP} | Inputs]) ->
 	file:close(FP),
@@ -44,12 +35,24 @@ filter_burst_config(Config) ->
 	[Item || Item <- Config,
 	 element(1, Item) =:= postfix orelse element(1, Item) =:= http_cfg].
 
-process_url_restriction([{url_restriction, Restriction} | Config]) ->
+process_user_config(Config) -> process_user_config(Config, [], []).
+process_user_config([], InputsAcc, ConfigAcc) -> {InputsAcc, ConfigAcc};
+
+%% open input files (wordlists, URL lists)
+process_user_config([{Type, FileName} | Config], InputsAcc, ConfigAcc)
+  when Type =:= wordlist; Type =:= url_list ->
+	{ok, FP} = file:open(FileName, [read, raw, read_ahead, binary]),
+	process_user_config(Config, [{Type, FP} | InputsAcc], ConfigAcc);
+
+%% compile URL restriction regular expressions
+process_user_config([{url_restriction, Restriction} | Config], InputsAcc, ConfigAcc) ->
 	Value = case Restriction of
 				X when is_list(X) ->
 					{ok, RE} = re:compile(X),
 					fun(URL) -> re:run(URL, RE, [{capture, none}]) =:= match end
 			end,
-	[{url_restriction, Value} | Config];
-process_url_restriction([Item | Config]) -> [Item | process_url_restriction(Config)];
-process_url_restriction([]) -> [].
+	process_user_config(Config, InputsAcc, [{url_restriction, Value} | ConfigAcc]);
+
+%% leave everything unchanged
+process_user_config([Item | Config], InputsAcc, ConfigAcc) ->
+	process_user_config(Config, InputsAcc, [Item | ConfigAcc]).
