aad7d3a7ed6ecb7f0e2f1f0ab01c0cf6578128b3
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-29 17:12:15 +0100

    added session list to web interface

--- priv/static/js/dirbusterl.js
+++ priv/static/js/dirbusterl.js
@@ -3,6 +3,33 @@ function start_bust(e) {
 	alert("TODO: bust");
 }
 
+function load_sessions() {
+	$.ajax({
+		url: "/bust",
+		dataType: "json",
+		success: load_sessions_data
+	});
+}
+
+function load_sessions_data(sessions) {
+	var tbody = $("#sessions tbody")
+	$(sessions).each(function (n, session) {
+		var tr = document.createElement("tr");
+		var id = document.createElement("td");
+		var url = document.createElement("td");
+		var a = document.createElement("a");
+		id.className = "bust_id";
+		id.appendChild(document.createTextNode(session.id));
+		a.appendChild(document.createTextNode(session.url));
+		a.href = session.url;
+		url.appendChild(a);
+		tr.appendChild(id);
+		tr.appendChild(url);
+		tbody.append(tr);
+	});
+}
+
 $(function() {
     $("#bust").submit(start_bust);
+	load_sessions();
 });
