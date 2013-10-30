e4d5844b8d382ad6ce29ca610e2d5832c81fe386
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-30 23:17:18 +0100

    implemented POST /bust endpoint

--- src/bust_resource.erl
+++ src/bust_resource.erl
@@ -1,5 +1,5 @@
 -module(bust_resource).
--export([init/1, content_types_provided/2, to_json/2]).
+-export([init/1, content_types_provided/2, to_json/2, content_types_accepted/2, allowed_methods/2, from_json/2, post_is_create/2, create_path/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 -define(CT_JSON, {"application/json", to_json}).
@@ -10,9 +10,58 @@ init([]) -> {ok, undefined}.
 content_types_provided(ReqData, State) ->
     {[?CT_JSON], ReqData, State}.
 
+content_types_accepted(ReqData, State) ->
+    {[{"application/json", from_json}], ReqData, State}.
+
+allowed_methods(ReqData, State) ->
+    {['HEAD', 'GET', 'POST'], ReqData, State}.
+
+post_is_create(ReqData, State) ->
+    {true, ReqData, State}.
+
+create_path(R, S) ->
+    {base64:encode_to_string(dirbusterl_storage:generate_bust_id()), R, S}.
+
 to_json(ReqData, State) ->
     Payload = case wrq:disp_path(ReqData) of
         "" -> dirbusterl_storage:get_busts();
         BustId -> dirbusterl_storage:get_findings(base64:decode(BustId))
     end,
     {mochijson2:encode(Payload), ReqData, State}.
+
+from_json(ReqData, State) ->
+    Id = base64:decode(wrq:disp_path(ReqData)),
+    {struct, Values} = mochijson2:decode(wrq:req_body(ReqData)),
+    {URL, Config} = process_json_values(Values),
+    dirbusterl:bust_async(Id, URL, Config),
+    {true, ReqData, State}.
+
+-define(AK(X), Key =:= X).
+-define(ALLOWED_KEY,
+        ?AK(<<"follow_dirs">>) ;
+        ?AK(<<"follow_redirs">>) ;
+        ?AK(<<"parse_body">>) ;
+        ?AK(<<"url_restriction">>) ;
+        ?AK(<<"postfix">>) ;
+        ?AK(<<"mangle_found">>) ;
+        ?AK(<<"http_cfg">>) ;
+        ?AK(<<"proxy_host">>) ;
+        ?AK(<<"proxy_port">>) ; %% TODO add more ibrowse atoms
+        ?AK(<<"wordlist">>) ;
+        ?AK(<<"url_list">>)).
+
+process_json_values(Values) -> process_json_values(Values, no_url, []).
+process_json_values([], URL, CfgAcc) -> {URL, CfgAcc};
+process_json_values([{<<"url">>, URL} | Values], _, CfgAcc) ->
+    process_json_values(Values, binary_to_list(URL), CfgAcc);
+process_json_values([{Key, Value} | Values], URL, CfgAcc) when ?ALLOWED_KEY ->
+    Entry = {list_to_atom(binary_to_list(Key)), process_json_value(Value)},
+    process_json_values(Values, URL, [Entry | CfgAcc]).
+
+process_json_value({struct, Values}) ->
+    {no_url, Processed} = process_json_values(Values),
+    Processed;
+process_json_value(Value) when is_binary(Value) ->
+    binary_to_list(Value);
+process_json_value(Value) when is_list(Value) ->
+    lists:map(fun process_json_value/1, Value).
