06bcd7297ec2a993e3e174651bcc870f271bdf06
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-31 13:21:25 +0100

    wordlist_resource: extracted functions wordlist{s,_dir}

--- src/wordlist_resource.erl
+++ src/wordlist_resource.erl
@@ -1,5 +1,5 @@
 -module(wordlist_resource).
--export([init/1, content_types_provided/2, to_json/2]).
+-export([init/1, content_types_provided/2, to_json/2, wordlist_dir/0, wordlists/0]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 -define(CT_JSON, {"application/json", to_json}).
@@ -10,7 +10,13 @@ init([]) -> {ok, undefined}.
 content_types_provided(ReqData, State) ->
     {[?CT_JSON], ReqData, State}.
 
+wordlist_dir() -> filename:join(code:priv_dir("dirbusterl"), "wordlists").
+
+wordlists() ->
+    {ok, Files} = file:list_dir(wordlist_dir()),
+    lists:foldl(fun (".gitignore", A) -> A;
+                    (E, A) -> ordsets:add_element(list_to_binary(E), A) end,
+                ordsets:new(), Files).
+
 to_json(ReqData, State) ->
-    {ok, Files} = file:list_dir(filename:join(code:priv_dir("dirbusterl"), "wordlists")),
-    WLs = lists:sort(Files -- [".gitignore"]),
-    {mochijson2:encode(lists:map(fun list_to_binary/1, WLs)), ReqData, State}.
+    {mochijson2:encode(wordlists()), ReqData, State}.
