b328dd1c711ef14a06302d455b3f1150f408fb29
András Veres-Szentkirályi <vsza@vsza.hu>
2014-01-07 13:04:17 +0100

    web: added checkboxes for boolean parameters

--- priv/static/js/dirbusterl.js
+++ priv/static/js/dirbusterl.js
@@ -4,6 +4,9 @@ function start_bust(e) {
 	$.each(["url", "wordlist"], function(n, e) {
 		data[e] = document.getElementById(e).value;
 	});
+	$.each(["follow_dirs", "follow_redirs", "parse_body"], function(n, e) {
+		if (document.getElementById(e).checked) data[e] = true;
+	});
 	$.ajax({
 		url: "/bust",
 		type: "POST",
