382926d3da7d05be44f9c69ed72905f84b8348fa
András Veres-Szentkirályi <vsza@vsza.hu>
2014-01-17 16:10:33 +0100

    added BV macro to dirbusterl_visited_urls_tests

--- src/dirbusterl_visited_urls_tests.erl
+++ src/dirbusterl_visited_urls_tests.erl
@@ -1,19 +1,21 @@
 -module(dirbusterl_visited_urls_tests).
 -include_lib("eunit/include/eunit.hrl").
 
+-define(BV(U), dirbusterl_visited_urls:book_visit(P, (U))).
+
 basic_test() ->
 	{ok, P} = dirbusterl_visited_urls:start_link(),
-	?assert(dirbusterl_visited_urls:book_visit(P, "http://localhost/foo")),
-	?assertNot(dirbusterl_visited_urls:book_visit(P, "http://localhost/foo")),
-	?assertNot(dirbusterl_visited_urls:book_visit(P, "http://localhost/foo")),
-	?assert(dirbusterl_visited_urls:book_visit(P, "http://localhost/foo/bar/qux/")),
-	?assertNot(dirbusterl_visited_urls:book_visit(P, "http://localhost/foo")),
-	?assertNot(dirbusterl_visited_urls:book_visit(P, "http://localhost/foo/bar/qux/")),
-	?assert(dirbusterl_visited_urls:book_visit(P, "http://localhost/foo/bar/qux")),
+	?assert(?BV("http://localhost/foo")),
+	?assertNot(?BV("http://localhost/foo")),
+	?assertNot(?BV("http://localhost/foo")),
+	?assert(?BV("http://localhost/foo/bar/qux/")),
+	?assertNot(?BV("http://localhost/foo")),
+	?assertNot(?BV("http://localhost/foo/bar/qux/")),
+	?assert(?BV("http://localhost/foo/bar/qux")),
 	dirbusterl_visited_urls:stop(P).
 
 premature_stop_test() ->
 	{ok, P} = dirbusterl_visited_urls:start_link(),
-	?assert(dirbusterl_visited_urls:book_visit(P, "http://localhost/foo")),
+	?assert(?BV("http://localhost/foo")),
 	dirbusterl_visited_urls:stop(P),
-	?assertExit({noproc, _}, dirbusterl_visited_urls:book_visit(P, "http://localhost/foo")).
+	?assertExit({noproc, _}, ?BV("http://localhost/foo")).
