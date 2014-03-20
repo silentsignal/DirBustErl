6e96d21a213f8085fbb34186fad5ef033024634f
András Veres-Szentkirályi <vsza@vsza.hu>
2014-03-20 16:07:29 +0100

    extracted functions worker:try_url_sync/{2,3,4}

--- src/worker.erl
+++ src/worker.erl
@@ -1,16 +1,23 @@
 -module(worker).
--export([try_url/3]).
+-export([try_url/4, try_url_sync/2]).
 
 -include_lib("eunit/include/eunit.hrl").
 
 -define(TRIES, 8).
 
-try_url(URL, Waiter, Params) -> try_url(URL, Waiter, Params, head).
-try_url(URL, Waiter, Params, Method) -> try_url(URL, Waiter, Params, Method, ?TRIES).
-try_url(URL, Waiter, Params, Method, N) ->
+try_url(URL, Waiter, Params, FailCase) ->
+	case try_url_sync(URL, Params) of
+		not_found -> waiter:worker_finished(Waiter);
+		FailCase -> waiter:worker_finished(Waiter);
+		{Result, Payload} -> waiter:worker_finished(Waiter, URL, Result, Payload)
+	end.
+
+try_url_sync(URL, Params) -> try_url_sync(URL, Params, head).
+try_url_sync(URL, Params, Method) -> try_url_sync(URL, Params, Method, ?TRIES).
+try_url_sync(URL, Params, Method, N) ->
 	case ibrowse:send_req(URL, [], Method, [], Params, infinity) of
-		{ok, "404", _, _} -> waiter:worker_finished(Waiter);
-		{ok, _, _, _} when Method =:= head -> try_url(URL, Waiter, Params, get);
+		{ok, "404", _, _} -> not_found;
+		{ok, _, _, _} when Method =:= head -> try_url_sync(URL, Params, get);
 		{ok, Code, Headers, Body} ->
 			Payload = case get_location(Headers) of
 				no_location -> Body;
@@ -20,11 +27,12 @@ try_url(URL, Waiter, Params, Method, N) ->
 						false -> {redir, Location}
 					end
 			end,
-			waiter:worker_finished(Waiter, URL, Code, Payload);
-		{error, retry_later} -> timer:sleep(100), try_url(URL, Waiter, Params, Method, N);
-		{error, _} when N > 0 -> try_url(URL, Waiter, Params, Method, N - 1);
-		{error, Reason} -> waiter:worker_finished(Waiter, URL, error, Reason)
+			{Code, Payload};
+		{error, retry_later} -> timer:sleep(100), try_url_sync(URL, Params, Method, N);
+		{error, _} when N > 0 -> try_url_sync(URL, Params, Method, N - 1);
+		{error, _} = Error -> Error
 	end.
+
 
 get_location([]) -> no_location;
 get_location([{Header, Value} | Rest]) ->
