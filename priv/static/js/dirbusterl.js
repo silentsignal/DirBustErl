52b209ee738459362b7e1058553bdd6640cc6aaa
András Veres-Szentkirályi <vsza@vsza.hu>
2014-04-24 14:53:59 +0200

    indent fix

--- priv/static/js/dirbusterl.js
+++ priv/static/js/dirbusterl.js
@@ -1,5 +1,5 @@
 function start_bust(e) {
-    e.preventDefault();
+		e.preventDefault();
 	data = new Object();
 	$.each(["url", "wordlist", "url_restriction"], function(n, e) {
 		var value = document.getElementById(e).value;
@@ -384,7 +384,7 @@ function fill_manglings() {
 }
 
 $(function() {
-    $("#bust").submit(start_bust);
+		$("#bust").submit(start_bust);
 	load_wordlists();
 	load_sessions();
 	fill_postfixes();
